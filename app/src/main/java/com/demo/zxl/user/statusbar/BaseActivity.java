4368db2f5855f0fe1e36c2ab91846abd20c242c1
zxiaolongo <zxiaolongo@163.com>
2018-09-11 20:26:14 +0800

    main界面初始化

---
+++ app/src/main/java/com/demo/zxl/user/statusbar/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.demo.zxl.user.statusbar;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.demo.zxl.user.statusbar.util.StatusBarUtil;
+
+
+/**
+ * Created by Jaeger on 16/2/14.
+ *
+ * Email: chjie.jaeger@gmail.com
+ * GitHub: https://github.com/laobie
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        setStatusBar();
+    }
+
+    protected void setStatusBar() {
+        StatusBarUtil.setColor(this, getResources().getColor(R.color.colorPrimary));
+    }
+}
